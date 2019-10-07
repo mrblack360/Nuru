6f280de5d910d82159a7f09abd69a4d6e05ce6b7
mrblack360 <rmaswi360@outlook.com>
2019-10-07 13:57:59 +0300

    feat(HomePageComponent): Added A datePickerComponent

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
   constructor() {}
 
+  presentPopover(ev) {
+    const popover = Object.assign(document.createElement('ion-popover'), {
+      component: 'popover-example-page',
+      event: ev,
+      translucent: true
+    });
+    document.body.appendChild(popover);
+    return popover.present();
+  }
 }
