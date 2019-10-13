50ca8eb6d36ac5dcaea0cd5c6dace9efd7525dd9
mrblack360 <rmaswi360@outlook.com>
2019-10-13 19:35:09 +0300

    feat(HopmePage Component): Added the quartery service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -20,6 +21,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     IonicModule.forRoot(),
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { MorePageComponent } from './pages/more-page/more-page.component';
 import { DatePicker } from '@ionic-native/date-picker/ngx';
 import { Platform } from '@ionic/angular';
 import { DatePipe } from '@angular/common';
-import { FormControl } from '@angular/forms';
-
+import { QuarteryService } from './../services/quartery.service';
+import { Quarterly } from 'src/shared/interfaces/quarterly';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  quarteries: Quarterly;
+  quarterColor: string;
   constructor(
     public popoverController: PopoverController,
     private datePicker: DatePicker,
     public datePipe: DatePipe,
-    public platform: Platform
+    public platform: Platform,
+    public quarteries$: QuarteryService
   ) {}
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+    this.quarteries$
+      .getQuarteries()
+      .subscribe(quarteries => (this.quarteries = quarteries));
+    this.quarterColor = this.quarteries.quarterColor;
+  }
   async morePagePopover(ev) {
     const popover = await this.popoverController.create({
       component: MorePageComponent,
