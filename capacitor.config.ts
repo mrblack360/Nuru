5e0efab680664c51c5e97070291835cc80350a4d
Maswi MR <rmaswi360@outlook.com>
2024-04-03 16:16:50 +0300

    ref: revamped the project Nuru

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.nuru',
   appName: 'nuru',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
