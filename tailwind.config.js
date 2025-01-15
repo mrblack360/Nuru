77ab89393a301dcf0b1c23add84cbd25261feaef
Maswi, MR <rmaswi360@outlook.com>
2025-01-15 20:20:30 +0300

    changed styles from bootstrap to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}", "./node_modules/@ionic/angular/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
